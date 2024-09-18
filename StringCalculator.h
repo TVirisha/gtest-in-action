f0f401eb4cf65d96124d41e5f3c85726708c8e2f
TVirisha <virisha94@gmail.com>
2024-09-18 14:31:25 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,10 +1,11 @@
+#include<iostream>
 #include<string>
 #include <vector>
 #include <numeric>
 #include <regex>
-#include <iostream>
 
 using namespace std;
 
-//int Add(const char * input);
-void Add(const string &input);
+//int Add(const char* input);
+//void Add(const string &input);
+void  Add(const string &input,void(*displayPtr)(int));
