eb02826e8072c4b9e729d060586dc9c2b8e40ccf
TVirisha <virisha94@gmail.com>
2024-09-18 11:31:53 +0530

    Update StringCalculatorDataDriven.Tests.cpp

--- StringCalculatorDataDriven.Tests.cpp
+++ StringCalculatorDataDriven.Tests.cpp
@@ -37,22 +37,19 @@ TEST_F(StringCalculatorDataDrivenFixture,DataDrivenTestCase){
 }
 TEST(StringCalculatorDataDrivenTestSuite,DataDrivenTestCase){
 
-  TestDataPair pair_one { "",0};
-  TestDataPair pair_two { "0",0};
-  TestDataPair pair_three { "1",1};
-  TestDataPair pair_four { "1,2",3};
-  TestDataPair pair_five { "1,2,3",6};
+    std::vector<TestDataPair*> dataList;
 
-  dataList.push_back(pair_one);
-  dataList.push_back(pair_two);
-  dataList.push_back(pair_three);
-  dataList.push_back(pair_four);
-  dataList.push_back(pair_five);
-  
-  //Iterate using a range-based for-loop
-    for (TestDataPair dataPair : dataList) {
-        int actualValue=Add(dataPair.input);
-        ASSERT_EQ(actualValue,dataPair.expectedValue);
+    // Dynamically allocate TestDataPair objects
+    dataList.push_back(new TestDataPair("", 0));
+    dataList.push_back(new TestDataPair("0", 0));
+    dataList.push_back(new TestDataPair("1", 1));
+    dataList.push_back(new TestDataPair("1,2", 3));
+    dataList.push_back(new TestDataPair("1,2,3", 6));
+
+    // Iterate using a range-based for-loop
+    for (TestDataPair* dataPair : dataList) {
+        int actualValue = Add(dataPair->input);
+        ASSERT_EQ(actualValue, dataPair->expectedValue);
     }
 
 }
